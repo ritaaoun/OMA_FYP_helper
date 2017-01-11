c9a660feb24e5f7697d91433d1bf97fb93580109
Rita Aoun <rita-aoun@hotmail.com>
2017-01-11 19:07:30 +0200

    added variable ngram threshold

--- src/ExtractNgramsMain.java
+++ src/ExtractNgramsMain.java
@@ -5,16 +5,27 @@ import java.util.HashMap;
 
 public class ExtractNgramsMain {
 	public static void main(String[] args) {
-		extractNgramsFromTrainSet();
+		int threshold = 3;
+		extractNgramsFromTrainSet(true, threshold);
+		extractNgramsFromTrainSet(false, threshold);
 	}
-	public static void extractNgramsFromTrainSet() {
+	public static void extractNgramsFromTrainSet(boolean lemmatized, int frequencyThreshold) {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("resources\\train tweets lemmatized.txt"));
-			PrintWriter uni = new PrintWriter("resources\\unigrams.txt", "UTF-8");
-			PrintWriter bi = new PrintWriter("resources\\bigrams.txt", "UTF-8");
-			PrintWriter tri = new PrintWriter("resources\\trigrams.txt", "UTF-8");
-			
-			extractNgramsFromFile(br,uni,bi,tri);
+			BufferedReader br;
+			PrintWriter uni, bi, tri;
+			if (lemmatized) {
+				br = new BufferedReader(new FileReader("resources\\train tweets lemmatized.txt"));
+				uni = new PrintWriter("resources\\unigrams.txt", "UTF-8");
+				bi = new PrintWriter("resources\\bigrams.txt", "UTF-8");
+				tri = new PrintWriter("resources\\trigrams.txt", "UTF-8");
+			}
+			else {
+				br = new BufferedReader(new FileReader("resources\\train tweets.txt"));
+				uni = new PrintWriter("resources\\unigrams_not_lemmatized.txt", "UTF-8");
+				bi = new PrintWriter("resources\\bigrams_not_lemmatized.txt", "UTF-8");
+				tri = new PrintWriter("resources\\trigrams_not_lemmatized.txt", "UTF-8");
+			}
+			extractNgramsFromFile(br,uni,bi,tri,frequencyThreshold);
 			
 			br.close();
 			uni.close();
@@ -25,7 +36,7 @@ public class ExtractNgramsMain {
 			e.printStackTrace();
 		}
 	}
-	public static void extractNgramsFromFolds() {
+	public static void extractNgramsFromFolds(int frequencyThreshold) {
 		try {
 			for (int fold=1; fold<=10; ++fold) {
 				BufferedReader br = new BufferedReader(new FileReader("ngrams\\fold"+fold+"\\train.txt"));
@@ -33,7 +44,7 @@ public class ExtractNgramsMain {
 				PrintWriter bi = new PrintWriter("ngrams\\fold"+fold+"\\bigrams.txt", "UTF-8");
 				PrintWriter tri = new PrintWriter("ngrams\\fold"+fold+"\\trigrams.txt", "UTF-8");
 
-				extractNgramsFromFile(br,uni,bi,tri);
+				extractNgramsFromFile(br,uni,bi,tri,frequencyThreshold);
 				
 				br.close();
 				uni.close();
@@ -46,7 +57,7 @@ public class ExtractNgramsMain {
 		}
 	}
 	
-	private static void extractNgramsFromFile(BufferedReader input, PrintWriter uni, PrintWriter bi, PrintWriter tri) {
+	private static void extractNgramsFromFile(BufferedReader input, PrintWriter uni, PrintWriter bi, PrintWriter tri, int frequencyThreshold) {
 		try {
 			HashMap<String, Integer> unigrams = new HashMap<String, Integer>();
 			HashMap<String, Integer> bigrams = new HashMap<String, Integer>();
@@ -80,19 +91,19 @@ public class ExtractNgramsMain {
 			}				
 			
 			for (String unigram : unigrams.keySet()) {
-				if (unigrams.get(unigram)>=5) {
+				if (unigrams.get(unigram)>=frequencyThreshold) {
 					uni.println(unigram);
 				}
 			}
 			
 			for (String bigram : bigrams.keySet()) {
-				if (bigrams.get(bigram)>=5) {
+				if (bigrams.get(bigram)>=frequencyThreshold) {
 					bi.println(bigram);
 				}
 			}
 			
 			for (String trigram : trigrams.keySet()) {
-				if (trigrams.get(trigram)>=5) {
+				if (trigrams.get(trigram)>=frequencyThreshold) {
 					tri.println(trigram);
 				}
 			}
